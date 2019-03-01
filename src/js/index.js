d2734799a39cf413ab825e4097628254a53f7229
zdx0823 <15916965182@163.com>
2019-03-01 17:49:06 +0800

    +不兼容的响应式已做好

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 $(function(){
 
+    var side_wrap = $('.side_wrap');
+    var main_wrap = $('.main_wrap');
+    var side_large = $('.side_large');
+    var side_small = $('.side_small');
+
 
 
 });
